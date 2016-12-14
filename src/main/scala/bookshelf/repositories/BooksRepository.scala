117f7ebbc6656c328811e1ee20e80c7272125e09
Marcus Almeida <marcusmage@gmail.com>
2016-12-14 18:07:29 -0200

    Fixing the repository

--- src/main/scala/bookshelf/repositories/BooksRepository.scala
+++ src/main/scala/bookshelf/repositories/BooksRepository.scala
@@ -1,11 +1,14 @@
 package bookshelf.repositories
 
-import java.lang.{Long => LongJava}
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import java.lang.{Long => JavaLong}
+import org.springframework.data.rest.core.annotation.{RepositoryRestResource, RestResource}
 import org.springframework.data.repository.PagingAndSortingRepository
-
+import org.springframework.data.repository.query.Param
 import bookshelf.entities.Book
 
 @RepositoryRestResource(collectionResourceRel = "books", path = "books")
-trait BooksRepository extends PagingAndSortingRepository[Book, LongJava] {
+trait BooksRepository extends PagingAndSortingRepository[Book, JavaLong] {
+  
+  @RestResource(path= "isbn", rel = "isbn")
+  def findByIsbn(@Param("isbn") isbn : String): Book
 }
